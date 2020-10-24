dd18369102c043d138a537ed445d4e43636ae39b
IoanLT <ioan.tranole@gmail.com>
2020-10-24 15:29:55 +0100

    Tried different examples and played with object methods

--- script.js
+++ script.js
@@ -1,40 +1,111 @@
-const person1 = {
-    name: 'Nelu',
-    age: 30,
-    sayHello: function(){
-        console.log(`Hello ${this.name}`);
-    }
-}
-console.log(person1.sayHello());
+// const person1 = {
+//     name: 'Nelu',
+//     age: 30,
+//     sayHello: function(){
+//         console.log(`Hello ${this.name}`);
+//     }
+// }
+// console.log(person1.sayHello());
 
-let instructor = {
-    name: 'Marc',
-    age: 30,
-    hasDog: true,
-    from: 'Barcelona',
-    dog: {
-        name: 'lulu',
-        age: 4,
-        isCrazy: true
-    },
-    hobbies: ['ski', 'surf', 'climbing']
-}
+// let instructor = {
+//     name: 'Marc',
+//     age: 30,
+//     hasDog: true,
+//     from: 'Barcelona',
+//     dog: {
+//         name: 'lulu',
+//         age: 4,
+//         isCrazy: true
+//     },
+//     hobbies: ['ski', 'surf', 'climbing']
+// }
 
-console.log(instructor);
+// console.log(instructor);
 
-let userChoice = {
-    sum: (num1, num2) => {
-        return num1 + num2;
-    },
-    multiply: (num1, num2) => {
-        return num1 * num2;
-    },
-    divide: (num1, num2) => {
-        return num1 / num2;
-    }    
-}
+// let userChoice = {
+//     sum: (num1, num2) => {
+//         return num1 + num2;
+//     },
+//     multiply: (num1, num2) => {
+//         return num1 * num2;
+//     },
+//     divide: (num1, num2) => {
+//         return num1 / num2;
+//     }    
+// }
 
-console.log(userChoice.sum(5, 8));
+// console.log(userChoice.sum(5, 8));
+
+
+// Math object
+
+// console.log(Math);
+// console.log(Math.PI);
+
+// const area = 7.4;
+
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+
+// const random = Math.random();
+// console.log(random);
+
+// console.log(Math.ceil(Math.random() * 100));
+
+// primitive values
+// let scoreOne = 50;
+// let scoreTwo = scoreOne;
+
+// console.log(`scoreOne: ${scoreOne}, scoreTwo: ${scoreTwo}`);
+
+// scoreOne = 100;
+// console.log(`scoreOne: ${scoreOne}, scoreTwo: ${scoreTwo}`);
+
+
+
+// reference values
+const user1 = {
+    name: 'ryu',
+    age: 50
+  }
+const user2 = user1;
+  
+console.log(user1, user2);
+  
+user2.age = 60;
+console.log(user1, user2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
